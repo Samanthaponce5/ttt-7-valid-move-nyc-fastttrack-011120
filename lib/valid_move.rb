fb986898e575900cb664c1b98ce6c636dbad72f3
Samantha Ponce <samanthaponce5@gmail.com>
2020-01-12 17:42:35 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,15 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  
-    if index.between?(0, 8) == true
+  def position_taken?(array, ind)
+    if array[ind] == " " || array[ind] == "" || array[ind] == nil
+      return false
+    else
+      return true
+    end
+  end
+
+  def on_board?(num)
+    if num.between?(0, 8) == true
       return true
     else
       return false
